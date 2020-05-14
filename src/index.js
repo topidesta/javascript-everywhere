26ff986652acf010d257f0c09df5c3fb026265f0
Desta F Muchamad <desta.08b@gmail.com>
2020-05-14 10:43:41 +0700

    #Refactoring Code Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 
+// load model
+const models = require("./models");
 const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
 require("dotenv").config();
 const db = require("./db");
@@ -9,26 +12,11 @@ const db = require("./db");
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 
-// load model
-const models = require("./models");
-
 let notes = [
   { id: "1", content: "The First Content", author: "Topidesta" },
   { id: "2", content: "The Second Content", author: "Desta" },
   { id: "3", content: "The Third Content", author: "Fadilah" },
 ];
-
-// fungsi untuk menampilkan schema yang dibuat
-const resolvers = {
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: "Desta Fadilah Topidesta",
-      });
-    },
-  },
-};
 
 const app = express();
 
@@ -36,7 +24,14 @@ const app = express();
 db.connect(DB_HOST);
 
 // setup server apollo
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    // add db models
+    return { models };
+  },
+});
 
 // Applikasikan GraphQL middleware dan set ke /api
 server.applyMiddleware({ app, path: "/api" });
