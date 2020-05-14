2048563b48f6662ef3342fe738de8fc04bde8a57
Desta F Muchamad <desta.08b@gmail.com>
2020-05-14 08:53:33 +0700

    Testing GrapQL
    # Write your query or mutation here
    query {
      hello
    }

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
-const app = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => res.send("Hello world"));
+// buat sebuah skema dengan GraphQL
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// fungsi untuk menampilkan schema yang dibuat
+const resolvers = {
+  Query: {
+    hello: () => "Hello world",
+  },
+};
+
+const app = express();
+
+// setup server apollo
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Applikasikan GraphQL middleware dan set ke /api
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen({ port }, () =>
+  console.log(`Listening on port ${port}${server.graphqlPath}`)
+);
