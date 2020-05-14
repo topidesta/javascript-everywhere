502a9a7cc6882da0a92a1271723ec20d9885f051
Desta F Muchamad <desta.08b@gmail.com>
2020-05-14 10:04:08 +0700

    Added MonggoDB Driver

---
+++ src/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+module.exports = {
+  connect: (DB_HOST) => {
+    // Monggo driver
+    mongoose.set("userNewUrlParser", true);
+    // Use findOneAndUpdate() in place of findAndModify()
+    mongoose.set("useFindAndModify", false);
+    // Use createIndex() in place of ensureIndex()
+    mongoose.set("useCreateIndex", true);
+    // Use the new server discovery and monitoring engine
+    mongoose.set("useUnifiedTopology", true);
+    // Connect to the DB
+    mongoose.connect(DB_HOST);
+    // log error
+    mongoose.connection.on("error", (err) => {
+      console.log(err);
+      console.log("Monggo Connection Error");
+      process.exit();
+    });
+  },
+  close: () => {
+    mongoose.connection.close();
+  },
+};
