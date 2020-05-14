e71588514b28fab70761de907dd21d1a53cc473a
Desta F Muchamad <desta.08b@gmail.com>
2020-05-14 08:35:40 +0700

    Added extended

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => res.send("Hello world"));
 
-app.listen(4000, () => console.log("Listening on port 4000"));
+app.listen(port, () => console.log(`Listening on port ${port}`));
