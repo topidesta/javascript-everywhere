d28321b6df6f9ce39188195ff01a64f2994fbd0a
Desta F Muchamad <desta.08b@gmail.com>
2020-05-26 03:31:33 +0700

    Update Query, menyamakan dengan source

--- src/schema.js
+++ src/schema.js
@@ -2,39 +2,43 @@ const { gql } = require("apollo-server-express");
 
 module.exports = gql`
   scalar DateTime
-  type Query {
-    hello: String!
-    notes: [Note!]!
-    note(id: ID!): Note!
-    user(username: String!): User
-    users: [User!]!
-    me: User!
-    noteFeed(cursor: String): NoteFeed
-  }
+
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
     createdAt: DateTime!
     updatedAt: DateTime!
     favoritecount: Int!
     favoritedBy: [User!]
   }
+
   type User {
     id: ID!
     username: String!
     email: String!
-    avatar: String!
+    avatar: String
     notes: [Note!]!
     favorites: [Note!]!
   }
+
   type NoteFeed {
     notes: [Note]!
     cursor: String!
     hasNextPage: Boolean!
   }
+
+  type Query {
+    notes: [Note!]!
+    note(id: ID): Note!
+    user(username: String!): User
+    users: [User!]!
+    me: User!
+    noteFeed(cursor: String): NoteFeed
+  }
+
   type Mutation {
-    newNote(content: String!): Note!
+    newNote(content: String!): Note
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
     signUp(username: String!, email: String!, password: String!): String!
